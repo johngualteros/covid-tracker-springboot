10ced33fa26358af8de212c24f1570e6be77cf79
John Gualteros <gualterosjohn40@gmail.com>
2022-10-03 14:07:23 -0500

    Final commit

--- src/main/java/com/john/covidtrackerspringboot/CovidTrackerSpringbootApplication.java
+++ src/main/java/com/john/covidtrackerspringboot/CovidTrackerSpringbootApplication.java
@@ -2,12 +2,14 @@ package com.john.covidtrackerspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CovidTrackerSpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CovidTrackerSpringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CovidTrackerSpringbootApplication.class, args);
+    }
 
 }
